c56669e1d2e4a72b01bd64c29c103685558b0b50
Pieter <git@pieter-verweij.nl>
2019-02-08 15:47:00 +0100

    Added selection drag

--- DPPaint/Commands/Click/SelectCommand.cs
+++ DPPaint/Commands/Click/SelectCommand.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Foundation;
+using Windows.UI;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
 using DPPaint.Shapes;
 
 namespace DPPaint.Commands.Click
@@ -17,6 +20,8 @@ namespace DPPaint.Commands.Click
         public List<PaintBase> ShapeList { get; set; }
 
         private readonly ICanvasPage _page;
+        private Shape selectorSquare;
+        private Point _pointerStart;
 
         public SelectCommand(ICanvasPage page)
         {
@@ -25,26 +30,79 @@ namespace DPPaint.Commands.Click
 
         public void PointerPressedExecute()
         {
-            Point pointerLocation = PointerEventArgs.GetCurrentPoint(Canvas).Position;
-            foreach (PaintBase paintBase in ShapeList)
+            _pointerStart = PointerEventArgs.GetCurrentPoint(Canvas).Position;
+            selectorSquare = new Rectangle();
+
+            selectorSquare.Width = 1;
+            selectorSquare.Height = 1;
+            selectorSquare.SetValue(Canvas.LeftProperty, _pointerStart.X);
+            selectorSquare.SetValue(Canvas.TopProperty, _pointerStart.Y);
+
+            Brush brush = new AcrylicBrush()
             {
-                if ((pointerLocation.X > paintBase.X && pointerLocation.X < (paintBase.X + paintBase.Width)
-                     && (pointerLocation.Y > paintBase.Y && pointerLocation.Y < (paintBase.Y + paintBase.Height))))
-                {
-                    paintBase.Selected = !paintBase.Selected;
-                    _page.Draw();
-                    _page.UpdateList();
-                    break;
-                }
-            }
+                TintColor = Colors.Blue,
+                TintOpacity = 0.1
+            };
+
+            selectorSquare.Fill = brush;
+
+            Canvas.Children.Add(selectorSquare);
         }
 
         public void PointerReleasedExecute()
         {
+            double x = (double)selectorSquare.GetValue(Canvas.LeftProperty);
+            double y = (double)selectorSquare.GetValue(Canvas.TopProperty);
+            double xMax = x + selectorSquare.Width;
+            double yMax = y + selectorSquare.Height;
+
+            foreach (PaintBase paintBase in ShapeList)
+            {
+                double eX = paintBase.X;
+                double eY = paintBase.Y;
+                double eXMax = eX + paintBase.Width;
+                double eYMax = eY + paintBase.Height;
+
+                if (((eX > x && eX < xMax) && (eY > y && eY < yMax)) &&
+                    ((eXMax > x && eXMax < xMax) && (eYMax > y && eYMax < yMax)))
+                {
+                    paintBase.Selected = true;
+                }
+            }
+
+            Canvas.Children.Remove(selectorSquare);
+            selectorSquare = null;
+
+            _page.UpdateList();
+            _page.Draw();
         }
 
         public void PointerMovedExecute()
         {
+            if (PointerEventArgs.Pointer.IsInContact && selectorSquare != null)
+            {
+                Point currentPoint = PointerEventArgs.GetCurrentPoint(Canvas).Position;
+                Point difference = new Point(currentPoint.X - _pointerStart.X, currentPoint.Y - _pointerStart.Y);
+                if (difference.X < 0)
+                {
+                    selectorSquare.Width = difference.X * -1.0;
+                    selectorSquare.SetValue(Canvas.LeftProperty, _pointerStart.X + difference.X);
+                }
+                else
+                {
+                    selectorSquare.Width = difference.X;
+                }
+
+                if (difference.Y < 0)
+                {
+                    selectorSquare.Height = difference.Y * -1.0;
+                    selectorSquare.SetValue(Canvas.TopProperty, _pointerStart.Y + difference.Y);
+                }
+                else
+                {
+                    selectorSquare.Height = difference.Y;
+                }
+            }
         }
     }
 }
